0984e5ea1477bcd0ce0b17263246b80a35bded1b
Kevin Carpenter™️ <kevin@moderne.io>
2023-10-12 19:15:08 +0000

    refactor: Common static analysis issues
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.staticanalysis.CommonStaticAnalysis?organizationId=RGVmYXVsdA%3D%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- spring-session-core/src/test/java/org/springframework/session/aot/hint/servlet/HttpSessionSecurityRuntimeHintsTests.java
+++ spring-session-core/src/test/java/org/springframework/session/aot/hint/servlet/HttpSessionSecurityRuntimeHintsTests.java
@@ -84,7 +84,7 @@ class HttpSessionSecurityRuntimeHintsTests {
 	void aotFactoriesContainsRegistrar() {
 		boolean match = SpringFactoriesLoader.forResourceLocation("META-INF/spring/aot.factories")
 				.load(RuntimeHintsRegistrar.class).stream()
-				.anyMatch((registrar) -> registrar instanceof HttpSessionSecurityRuntimeHints);
+				.anyMatch(HttpSessionSecurityRuntimeHints.class::isInstance);
 		assertThat(match).isTrue();
 	}
 
